69c7751db3a85e751059f65e7d8e06fe1d1c25bc
jameshoo123 <hoosinyong@hotmail.com>
2022-04-08 15:55:16 +0800

    feat: add seeders

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+
 const app = express()
 const PORT = 3000
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
 
 app.get('/', (req, res) => {
   res.send('hello world')
@@ -26,7 +33,8 @@ app.get('/users/register', (req, res) => {
 })
 
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password }).then(user => res.redirect('/'))
 })
 
 app.get('/users/logout', (req, res) => {
