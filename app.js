e1ecf54af38ed190ab7504db942c42f8a4b05a98
jameshoo123 <hoosinyong@hotmail.com>
2022-04-08 16:07:52 +0800

    feat: add login authentication function

--- app.js
+++ app.js
@@ -2,19 +2,34 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const passport = require('passport')
 
 const app = express()
 const PORT = 3000
+
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+
+app.use(
+  session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true,
+  })
+)
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -33,9 +48,14 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+// 加入 middleware，驗證 reqest 登入狀態
+app.post(
+  '/users/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+  })
+)
 
 app.get('/users/register', (req, res) => {
   res.render('register')
